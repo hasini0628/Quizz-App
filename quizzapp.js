e29d20e0cd03739063979b66df9100b3a18f9f00
Hasini <hasinielikatti06@gmail.com>
2025-10-23 20:09:52 +0530

    Second commit

---
+++ quizzapp.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+      question: "What is the full form of HTML?",
+      options: ["Hyper Test Mark Language", "Hyper Text Maintainable Language", "Hyper Text Markup Language", "Heading Text Markup Language"],
+      answer: 2
+    },
+    {
+      question: "What is the full form of CSS?",
+      options: ["Cascading Style Sheet", "Color Styling Sheet", "Cascading Styling Sheet", "Cascading Style Server"],
+      answer: 0
+    },
+    {
+      question: "How do you add an image in HTML?",
+      options: ['<img src="" alt="">', '<image src="" alt="">', '<image source="" alt=">', '<img src="" alterante="">'],
+      answer: 0
+    },
+    {
+      question: "What is the correct method to add an css file in html file?",
+      options: ['<link alt="" href="">', '<link alternate="" href="">', '<script alt="" href="">', '<link ref="" alterante="">'],
+      answer: 0
+    },
